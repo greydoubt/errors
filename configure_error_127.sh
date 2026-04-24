d5eef7e1c7b23d973139db74f8debd92d941f126
λx . ophidia ereц grex⋆. 𐙚 ̊      .    أوفيديا إريكس جريكس   . <43443470+greydoubt@users.noreply.github.com>
2026-04-24 03:25:32 -0700

    Create configure_error_127.sh

---
+++ configure_error_127.sh
@@ -0,0 +1,23 @@
+
+Run actions/checkout@v4
+Syncing repository: greydoubt/s_lime
+Getting Git version info
+Temporarily overriding HOME='/home/runner/work/_temp/263c889e-6e69-4154-b9d1-266a83f70541' before making global git config changes
+Adding repository directory to the temporary git global config as a safe directory
+/usr/bin/git config --global --add safe.directory /home/runner/work/s_lime/s_lime
+Deleting the contents of '/home/runner/work/s_lime/s_lime'
+Initializing the repository
+Disabling automatic garbage collection
+Setting up auth
+Fetching the repository
+Determining the checkout info
+/usr/bin/git sparse-checkout disable
+/usr/bin/git config --local --unset-all extensions.worktreeConfig
+Checking out the ref
+/usr/bin/git log -1 --format=%H
+26c7d97912aba651b5fffbce27535491d4929fcc
+
+0s
+Run ./configure
+/home/runner/work/_temp/f1c81c68-bdbd-4f3b-8b9a-9c7178addcb6.sh: line 1: ./configure: No such file or directory
+Error: Process completed with exit code 127.
